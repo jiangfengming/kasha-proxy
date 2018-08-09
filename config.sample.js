66e4d897c255eff1edfadc1f08826629adad28c6
Jiang Fengming <jiangfengming87@gmail.com>
2018-08-09 19:47:49 +0800

    config file

--- config.sample.js
+++ config.sample.js
@@ -1,13 +1,38 @@
 module.exports = {
   port: 3100,
 
-  store: {
-
-  },
-
   sentry: {
     dsn: ''
   },
 
-  loglevel: 'debug' // debug, info, warning, error, fatal
+  loglevel: 'debug', // debug, info, warning, error, fatal
+
+  sites: {
+    'https://www.example.com:8000': {
+      upstream: 'https://bucket-foo.somecloud.com',
+      upstreamHeaders: {
+
+      },
+      realFileExtensions: ['html', 'js', 'css', 'jpg', 'jpeg', 'png', 'gif', 'eot', 'ttf', 'woff', 'woff2', 'svg', 'svgz'],
+      virtualPath: [
+        ['/:project/*', '/$1/index.html'],
+        ['*', '/index.html']
+      ],
+      kashaHeaders: {
+        token: 'aaaaaaaa'
+      }
+    }
+  },
+
+  // store sites in remote storage instead of using 'sites' config
+  sitesStore: {
+    adapter: 'mongodb',
+    options: {
+      url: 'mongodb://localhost:27017',
+      database: 'kasha',
+      options: {
+        poolSize: 10
+      }
+    }
+  }
 }
